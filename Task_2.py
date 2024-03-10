6ccdd7c73d97f22ad1ff00e285013323cc3ae60d
kczerwienka <czerwony2000@gmail.com>
2024-03-10 12:39:56 +0100

    Task_2 commit

--- Task_2.py
+++ Task_2.py
@@ -8,6 +8,28 @@ def add_contact(args, contacts):
     contacts[name] = phone
     return "Contact added."
 
+def change_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        contacts[name] = phone
+        return "Contact changed."
+    else:
+        return "Contact does not exist, unable to change."
+
+def phone_contact(args, contacts):
+    name, phone = args
+    if name in contacts:
+        return str(contacts[name])
+    else:
+        return "Contact does not exist, unable to call."
+    
+def all_contacts():
+    global contacts
+    str=""
+    for k, v in contacts.items:
+        str += ('{:<10} {:<10}\n'.format(k, v))
+    return str
+
 def main():
     contacts = {}
     print("Welcome to the assistant bot!")
@@ -22,6 +44,12 @@ def main():
             print("How can I help you?")
         elif command == "add":
             print(add_contact(args, contacts))
+        elif command == "change":
+            print(change_contact(args, contacts))
+        elif command == "phone":
+            print(phone_contact(args, contacts))
+        elif command == "all":
+            print(all_contacts())
         else:
             print("Invalid command.")
 
